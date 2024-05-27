2dbc1e3d9246791a91755a38776df6dffb5796ef
RollingTL <nurlan.bbjs@gmail.com>
2024-05-27 17:39:12 +0500

    base set

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/DateInputDemo/',
   plugins: [vue()],
   resolve: {
     alias: {
